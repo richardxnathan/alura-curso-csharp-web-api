d1935421c4d0e5855d1f57d28db67cf3bb7278f8
richardxnathan <richardnathand@gmail.com>
2024-02-11 15:53:50 -0300

    Aula 06

--- FilmesApi/FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -23,7 +23,17 @@ public class FilmeController : ControllerBase
         _mapper = mapper;
     }
 
+    /*
+     * <summary>
+     * Adiciona um filme ao banco de dados
+     * </summary>
+     * <param name="filmeDto">Objeto com os campos necessários para criação de um filme</param>
+     * <returns>IActionResult</returns>
+     * <response code="201">Caso inserção seja feita com sucesso</response>
+     */
+
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
     public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
     {
 
