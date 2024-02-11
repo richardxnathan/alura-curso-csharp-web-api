f5a833b23766942c9a1a989a01349f393049f0be
richardxnathan <richardnathand@gmail.com>
2024-02-11 15:05:29 -0300

    Aula 04

--- FilmesApi/FilmesApi/obj/Debug/net6.0/FilmesApi.AssemblyInfo.cs
+++ FilmesApi/FilmesApi/obj/Debug/net6.0/FilmesApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5473dbaf774516e247deb22d6c450e70cd1350cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
